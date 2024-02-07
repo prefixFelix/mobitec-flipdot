b9f3a3dd118c1b20e895007802de4c59f6ab3bdd
ByteSizedMarius <hello@marius.codes>
2024-02-07 19:38:49 +0100

    feat(go): Add DrawPath to Matrix Example

--- src/go/mobitec/examples/cfg.go
+++ src/go/mobitec/examples/cfg.go
@@ -1,5 +1,7 @@
 package cfg
 
+import "time"
+
 const (
 	WIDTH  = 28 * 4
 	HEIGHT = 16
@@ -10,4 +12,8 @@ func Check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Wait() {
+	time.Sleep(750 * WIDTH / 28 * time.Millisecond)
 }
--- src/go/mobitec/examples/matrix-manipulation/main.go
+++ src/go/mobitec/examples/matrix-manipulation/main.go
@@ -5,22 +5,14 @@ import (
 	"github.com/prefixFelix/mobitec-flipdot/src/go/mobitec"
 	. "github.com/prefixFelix/mobitec-flipdot/src/go/mobitec/examples"
 	"github.com/prefixFelix/mobitec-flipdot/src/go/mobitec/matrix"
-	"time"
 )
 
 var display *mobitec.Display
 
-func wait() {
-	time.Sleep(750 * WIDTH / 28 * time.Millisecond)
-
-}
-
-func toDisplay(m *matrix.Matrix) {
-	Check(display.Matrix(*m))
-	wait()
-}
-
 func main() {
+	// Set the display values in cfg.go
+	//
+	// Initialize the display, open the serial connection and make sure it is closed at the end
 	display = mobitec.NewDisplay(PORT, WIDTH, HEIGHT)
 	err := display.Open()
 	Check(err)
@@ -40,7 +32,10 @@ func main() {
 	toDisplay(m)
 
 	// Put a column of pixels into the matrix
-	m.SetColumn(WIDTH/2, true)
+	// SetValue is like choosing the color of your pen. It's the value most manipulation functions will use.
+	// True means dots will be set, false means they will be cleared. It's true by default.
+	m.SetValue(true)
+	m.SetColumn(WIDTH / 2)
 
 	// Set the matrix into the display (send the matrix to the display)
 	// All pixels that are not set will be cleared
@@ -61,7 +56,7 @@ func main() {
 	//
 
 	// Set a single pixel
-	m.Set(0, 0, true)
+	m.Set(0, 0)
 	toDisplay(m)
 
 	// Set a row/column to a slice of values
@@ -72,7 +67,7 @@ func main() {
 	toDisplay(m)
 
 	// Set a full row/column to a value
-	m.SetRow(HEIGHT/2, true)
+	m.SetRow(HEIGHT / 2)
 	toDisplay(m)
 
 	// Set a repeating pattern
@@ -91,18 +86,78 @@ func main() {
 	drawWidth := 14
 	drawFrom := WIDTH/2 - drawWidth/2
 	drawTo := drawFrom + drawWidth
+	m.SetValue(false)
 	for i := drawFrom - 1; i <= drawTo+1; i++ {
-		m.SetColumn(i, false)
+		m.SetColumn(i)
 	}
 	toDisplay(m)
 
 	// Draw a simple shape
+	m.SetValue(true)
 	for i := drawFrom; i < WIDTH/2; i++ {
-		m.SetColumnFromTo(i, i-drawFrom, HEIGHT-i+drawFrom, true)
+		m.SetColumnFromTo(i, i-drawFrom, HEIGHT-i+drawFrom)
 	}
 	for i := drawTo; i >= WIDTH/2; i-- {
-		m.SetColumnFromTo(i, (i-drawTo)*-1, HEIGHT+i-drawTo, true)
+		m.SetColumnFromTo(i, (i-drawTo)*-1, HEIGHT+i-drawTo)
 	}
+	toDisplay(m)
+	Wait()
+	Wait()
+
+	// Draw a rocket ship
+	ROCKETSHIP_LENGTH := WIDTH
+	m.Clear()
+
+	// Lower Part
+	m.DrawPath(0, 1, []matrix.Offset{
+		{X: 1},
+		{X: 1, Y: 1},
+		{X: 1},
+		{X: 1, Y: 1, Re: 2},
+		{Y: 1},
+		{X: -1, Y: 1, Re: 2},
+		{X: -1},
+		{X: -2, Y: 1},
+		{X: 1, Re: 3},
+		{X: -1, Y: 1},
+		{X: 1},
+		{X: 1, Y: 1, Re: 2},
+		{Y: 1},
+		{X: -1, Y: 1, Re: 2},
+		{X: -1},
+		{X: -1, Y: 1},
+		{X: -1},
+	})
+
+	end := ROCKETSHIP_LENGTH - 17
+	// Top: Bottom Part
+	m.SetRowFromTo(2, 4, end+5)
+	m.SetRowFromTo(14, 4, end+5)
+	m.DrawPath(end+4, 3, []matrix.Offset{
+		{X: -1, Y: 1},
+		{Y: 1, Re: 8},
+		{X: 1, Y: 1},
+	})
+
+	// Top: Top Part
+	m.DrawPath(end+5, 3, []matrix.Offset{
+		{X: 1, Re: 4},
+		{X: 1, Y: 1},
+		{X: 1},
+		{X: 1, Y: 1, Re: 3},
+		{X: -5, Y: 1},
+		{X: 1, Re: 5},
+		{Y: 1},
+		{X: -1, Y: 1, Re: 3},
+		{X: -1},
+		{X: -1, Y: 1},
+		{X: -1, Re: 4},
+	})
 
 	toDisplay(m)
+}
+
+func toDisplay(m *matrix.Matrix) {
+	Check(display.Matrix(*m))
+	Wait()
 }
